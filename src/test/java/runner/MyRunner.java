d4a4169c3d5091499be9c10742869575ab6f9ae5
catfishjim <jjnospam68@gmail.com>
2025-02-28 03:40:28 -0500

    n

--- src/test/java/runner/MyRunner.java
+++ src/test/java/runner/MyRunner.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-
 @RunWith(Cucumber.class)
 //@CucumberOptions(features= "src/test/resources/features/Login.feature",
 @CucumberOptions(features= "src/test/resources/features",
